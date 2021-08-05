64f8258f0055e15c1686b5042d6169a999f5d558
Phan Duc <1951060628@e.tlu.edu.vn>
2021-08-05 16:20:59 +0700

    initial commit

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from functools import total_ordering
 import time
 
+userName = input('Nhap ten muon tao: ')
+password = input('Nhap password muon tao: ')
+
 web = webdriver.Chrome()
 web.get('https://club.pokemon.com/us/pokemon-trainer-club/sign-up/')
-time.sleep(2)
+time.sleep(1)
 
 
 # Accept Cookies
@@ -13,7 +16,6 @@ acp = web.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
 acp.click()
 
 # Set dob
-
 # Open dob pop-up
 dob = web.find_element_by_xpath('//*[@id="id_dob"]')
 dob.click()
@@ -37,5 +39,36 @@ confirmDOB.click()
 continueToReg = web.find_element_by_xpath('//*[@id="sign-up-theme"]/section/div/div/div[1]/form/input[2]')
 continueToReg.click()
 
+# Fill username
+f = open("index.txt", "r")
+index = f.read()
+userN = web.find_element_by_xpath('//*[@id="id_username"]')
+finalUser = userName + str(index)
+# 
+if(userN.send_keys(finalUser) == True):
+    check = 0
+else:
+    check = 1
 
+# Plus index plus 1
+if(check == 1):
+    f = open("index.txt", "w")
+    intIndex = int(index)
+    newIndex = intIndex + 1
+    f.write(str(newIndex))
 
+# Password and confirm password
+passW = web.find_element_by_xpath('//*[@id="id_password"]')
+passW.send_keys(password)
+passW_cf = web.find_element_by_xpath('//*[@id="id_confirm_password"]')
+passW_cf.send_keys(password)
+
+# Email and confirm email
+email = web.find_element_by_xpath('//*[@id="id_email"]')
+email.send_keys(finalUser + '@hieucuoi.tech')
+email = web.find_element_by_xpath('//*[@id="id_confirm_email"]')
+email.send_keys(finalUser + '@hieucuoi.tech')
+
+# Accept the Pokemon.com Terms of Use.
+acceptTerm = web.find_element_by_xpath('//*[@id="id_terms"]')
+acceptTerm.click()
