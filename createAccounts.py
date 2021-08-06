2fe85493ff6d14c8d982058fbe35d60593a750b8
Phan Duc <55222003+hanngoc1406@users.noreply.github.com>
2021-08-06 23:04:06 +0700

    new way to set DOB to fix some error

--- createAccounts.py
+++ createAccounts.py
@@ -15,13 +15,10 @@ n = input('Nhap so tai khoan muon tao: ')
 t = int(n)
 i = int(index)
 
-
 while(t != 0):
 
     # Call webdriver
-    option = webdriver.ChromeOptions()
-    option.add_argument("--start-maximized")
-    web = webdriver.Chrome(chrome_options=option)
+    web = webdriver.Chrome()
     web.get('https://club.pokemon.com/us/pokemon-trainer-club/sign-up/')
     time.sleep(2)
 
@@ -29,26 +26,9 @@ while(t != 0):
     acp = web.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
     acp.click()
 
-    # Set dob
-    # Open dob pop-up
-    dob = web.find_element_by_xpath('//*[@id="id_dob"]')
-    dob.click()
-
-
-    # Open year option
-    yearPressed = web.find_element_by_xpath('//*[@id="id_dob_root"]/div/div/div/div/div[1]/div[1]/div/label')
-    yearPressed.click()
-
-    # Choose 1996 as the year of birth
-    
-    scrollToYear = web.find_element_by_xpath('//*[@id="id_dob_root"]/div/div/div/div/div[1]/div[1]/div/div/div/ul/li[26]')
-    time.sleep(2)
-    scrollToYear.location_once_scrolled_into_view
-    scrollToYear.click()
-
-    # Confirm DOB
-    confirmDOB = web.find_element_by_xpath('//*[@id="id_dob_root"]/div/div/div/div/div[2]/button')
-    confirmDOB.click()
+    # Set DOB
+    dob = web.find_element_by_xpath('//*[@id="sign-up-theme"]/section/div/div/div[1]/form/div[1]/input[2]')
+    web.execute_script("arguments[0].value='1996-01-10';", dob)
 
     # Continue to SignUp
     continueToReg = web.find_element_by_xpath('//*[@id="sign-up-theme"]/section/div/div/div[1]/form/input[2]')
@@ -81,6 +61,10 @@ while(t != 0):
     f.write(str(i))
     f.close()
 
+    info = open('Info.txt', 'w')
+    info.write(userName + index + ':' + password + ':' + userName + index + mail + '\n')
+    info.close()
+
     while (True):
         time.sleep(10)
         try:
@@ -91,6 +75,3 @@ while(t != 0):
             continue
     t -= 1
 
-f = open('Info.txt', 'w')
-f.write(userName + index + ':' + password + ':' + userName + index + mail + '\n')
-f.close()
