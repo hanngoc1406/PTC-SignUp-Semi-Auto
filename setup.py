3de180ab5e3b9ce3498db94d78a178f1e981ba5f
Phan Duc <55222003+hanngoc1406@users.noreply.github.com>
2021-08-06 20:52:52 +0700

    detel unnecessary file

--- setup.py
+++
@@ -1,6 +0,0 @@
-userName = input('Nhap ten muon tao: ')
-password = input('Nhap password muon tao: ')
-n = input('Nhap so tai khoan muon tao: ')
-
-for i in range(0, int(n)):
-    exec(open('createAccounts.py').read())
